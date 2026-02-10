c233412656d0b8076af4740bf83461d9cb40c88c
examonline202-ltt <examonline202@gmail.com>
2026-02-10 11:04:55 +0700

    Update supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://yydvhvwxysfolthcsnub.supabase.co';
-export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5ZHZodnd4eXNmb2x0aGNzbnViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0Nzg3MTIsImV4cCI6MjA4MjA1NDcxMn0.29stDaTxBKObubh308lKYpJslmxpra9Isc-mfhKgPjU';
+export const supabaseUrl = 'https://ibuekmzgwioscvgjptyw.supabase.co';
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlidWVrbXpnd2lvc2N2Z2pwdHl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2Njk0NTYsImV4cCI6MjA4NjI0NTQ1Nn0.C0YeAKvAc041u12JWBdaHo6qH_5vhdNQWWfqg6Jg0_w';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
